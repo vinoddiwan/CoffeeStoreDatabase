f272ec8f2b8a5ac0aed1686f4bfb7a4dd7e78b54
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-03 00:54:49 +0530

    Update coffee_store.sql

--- coffee_store.sql
+++ coffee_store.sql
@@ -1,1 +1,22 @@
+CREATE DATABASE coffee_store;
+
+USE DATABASE coffee_store;
+
+-- 3 tables Customers, Order, Products
+CREATE TABLE customers
+(
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+gender ENUM('M', 'F'),
+phone_number VARCHAR(11)
+);
+
+CREATE TABLE products
+(
+id INT AUTO INCREMENT PRIMARY KEY,
+name VARCHAR(30),
+price DECIMAL(3, 2)
+)
+
 
